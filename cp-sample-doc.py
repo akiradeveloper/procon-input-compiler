58b124057e35d02d420f029211844ce3745435cb
Akira Hayakawa <ruby.wktk@gmail.com>
2023-01-09 10:35:47 +0900

    upd

--- cp-sample-doc.py
+++ cp-sample-doc.py
@@ -4,11 +4,4 @@ import shutil
 
 DEST="../procon-input-support/example"
 shutil.rmtree(DEST)
-os.makedirs(DEST, exist_ok=True)
-l=[]
-for p in glob.glob("test-data/case/*/parser"):
-    l.append(p)
-l.sort()
-# print(l)
-for (i,p) in enumerate(l):
-    shutil.copyfile(p, DEST+'/'+str(i+1))
+shutil.copytree("test-data/case", DEST)
